ce8a8a2b172c6f4486faaaa2eb06b9076b60e2e6
Luis Rondon <rondonluisgrf@gmail.com>
2023-10-16 20:59:58 -0300

    feat: maquetacion del home para revision

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require("express")
-const path = require("path")
-const PORT = 3030
-const app = express()
+const express = require("express");
+const path = require("path");
+const PORT = 3030;
+const app = express();
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 
-app.listen(PORT, console.log("Server running in htt://localhost:" + PORT))
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, console.log("Server running in http://localhost:" + PORT))
